d684e67aed0a1c9e99529e0bc443330855543653
andre <andre@DESKTOP-9H97ONF>
2021-03-02 10:39:01 +0200

    Week 2, before the break

---
+++ Seminar2_JavaBasics/src/ro/ase/csie/cts/g1092/week2/exeptions/InsufficientFundsExceptions.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1092.week2.exeptions;
+
+public class InsufficientFundsExceptions extends Exception {
+	public InsufficientFundsExceptions(String message) {
+		super(message);
+	}
+	public InsufficientFundsExceptions() {
+		
+	}
+}
