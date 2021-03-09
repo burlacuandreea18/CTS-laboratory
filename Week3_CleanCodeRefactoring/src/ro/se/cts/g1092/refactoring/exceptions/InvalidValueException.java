11c1b3dfe329a170f56df13a3a470075e046c626
andre <andre@DESKTOP-9H97ONF>
2021-03-09 11:32:34 +0200

    Final Commit

---
+++ Week3_CleanCodeRefactoring/src/ro/se/cts/g1092/refactoring/exceptions/InvalidValueException.java
@@ -0,0 +1,5 @@
+package ro.se.cts.g1092.refactoring.exceptions;
+
+public class InvalidValueException extends Exception {
+
+}
--- Week3_CleanCodeRefactoring/src/ro/se/cts/g1092/refactoring/phase3/SalesStrategy.java
+++ Week3_CleanCodeRefactoring/src/ro/se/cts/g1092/refactoring/phase3/SalesStrategy.java
@@ -1,4 +1,4 @@
-package ro.se.cts.g1092.refactoring.phase2;
+package ro.se.cts.g1092.refactoring.phase3;
 
 import ro.se.cts.g1092.refactoring.exceptions.InvalidValueException;
 
@@ -6,6 +6,12 @@ public class SalesStrategy{
 	
 	public static final int FIDELITY_YEARS_THRESHOLD=10;
 	public static final float MAX_FIDELITY_DISCOUNT=0.15f;
+	
+	public static float getFidelityDiscount(int yearsSinceRegistration) {
+		return (yearsSinceRegistration>FIDELITY_YEARS_THRESHOLD)? MAX_FIDELITY_DISCOUNT: (float)yearsSinceRegistration/100;;
+	}
+
+	public static float getPriceDiscount(float initialPrice, ProductType,)
 	
 	public float ComputeFinalPrice(
 			ProductType productType, float initialPrice, int yearsSinceRegistration) throws InvalidValueException 
